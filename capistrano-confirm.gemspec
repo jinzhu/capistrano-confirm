f5cde6b4c4c16a24bacd955d69c62bec114c8ad0
Jinzhu <wosmvp@gmail.com>
2011-12-15 17:52:33 +0800

    Only confirm in some stages & Version bump to v0.0.2

--- capistrano-confirm.gemspec
+++ capistrano-confirm.gemspec
@@ -1,13 +1,18 @@
 # -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+require "capistrano-confirm/version"
 
 Gem::Specification.new do |s|
   s.name        = "capistrano-confirm"
-  s.version     = "0.0.1" #Capistrano::Confirm::VERSION
+  s.version     = Capistrano::Confirm::VERSION
   s.authors     = ["Jinzhu"]
   s.email       = ["wosmvp@gmail.com"]
   s.homepage    = "https://github.com/jinzhu/capistrano-confirm"
   s.summary     = %q{Confirm before deploy}
-  s.description = %q{Confirm before deploy}
+  s.description = %q{Confirm before deploy,
+                     Make sure you are not deploying the wrong branch, and remind you should deploy it after code review!
+                     Please use it, because I write it after deployed a wrong branch to our production server! ;(
+                    }
 
   s.rubyforge_project = "capistrano-confirm"
 
--- lib/capistrano-confirm.rb
+++ lib/capistrano-confirm.rb
@@ -5,30 +5,32 @@ end
 Capistrano::Configuration.instance.load do
   namespace :deploy do
     task :confirm do
-      first_num, sencond_num = rand(10), rand(10)
+      if (Array[confirm_stages].flatten.map(&:to_s).include?(stage.to_s) rescue true)
+        first_num, sencond_num = rand(10), rand(10)
 
-      result      = first_num + sencond_num
-      stage_info  = " #{colorful(stage)}" rescue ''
-      branch_info = " with branch #{colorful(branch)}" rescue ''
+        result      = first_num + sencond_num
+        stage_info  = " #{colorful(stage)}" rescue ''
+        branch_info = " with branch #{colorful(branch)}" rescue ''
 
-      Capistrano::CLI.ui.say %Q(
+        Capistrano::CLI.ui.say %Q(
 #{colorful("="*80, 33)}
 
 You are deploying#{stage_info}#{branch_info}, Are you sure?
 Please make sure you did #{colorful("code review")}, and are not deploying the #{colorful("wrong branch")},
-Answer the bellow question to make sure you are not drunk!
+Answer the below question to make sure you are not drunk!
   #{first_num} + #{sencond_num} = ?
 
 #{colorful("="*80, 33)}
 )
 
-      answer = Capistrano::CLI.ui.ask("Your answer here (answer `NO` to stop the deploy):").strip
-      if (answer =~ /^\s*\d+\s*$/) && (answer.to_i == result)
-        Capistrano::CLI.ui.say colorful("Deploying#{"."*71}", 33)
-      else
-        Capistrano::CLI.ui.say colorful("Stoping#{"."*73}", 33)
-        Capistrano::CLI.ui.say colorful("Difficult question for you? Your math must be taught by National Bureau of Statistics!") if (answer =~ /^\s*\d+\s*$/)
-        abort
+        answer = Capistrano::CLI.ui.ask("Your answer here (answer `NO` to stop the deploy):").strip
+        if (answer =~ /^\s*\d+\s*$/) && (answer.to_i == result)
+          Capistrano::CLI.ui.say colorful("Deploying#{"."*71}", 33)
+        else
+          Capistrano::CLI.ui.say colorful("Stoping#{"."*73}", 33)
+          Capistrano::CLI.ui.say colorful("Difficult question for you? Your math must be taught by National Bureau of Statistics!") if (answer =~ /^\s*\d+\s*$/)
+          abort
+        end
       end
     end
 
